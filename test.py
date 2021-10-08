bc6f4dcd861ab395b5524f67bf1c49f5d6c8db76
MusicWill <littlethings0630@gmail.com>
2021-10-08 16:36:38 -0400

    new file for basics of python

---
+++ test.py
@@ -0,0 +1,23 @@
+a = 100
+b = 1000
+c = 10000
+
+sum = a+b+c
+
+
+walking = "body health plus one"
+print(walking * 6)
+
+
+
+Max_is_healthy = True
+if Max_is_healthy:
+    print("Max_is_good")
+else:
+    print("Max_is_sick")
+
+
+while 1234<=1289:
+    print("good")
+else:
+    print("not good")
