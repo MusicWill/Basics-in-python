cf0a1242166583bdc87a01dcb348f107461d346f
MusicWill <littlethings0630@gmail.com>
2021-10-17 19:44:14 -0400

    Create strings and math functions.py

---
+++ strings and math functions.py
@@ -0,0 +1,40 @@
+str = "my name is Willis."
+print(str. capitalize())
+
+str = "Hello."
+print(str.casefold())
+
+str = "what is your name?"
+print(str.capitalize())
+
+message = 'Python is cool'
+print('Number of occurrence of o:', message.count('o'))
+
+print(message.startswith('Python'))
+
+print(message.lower())
+
+print(message.upper())
+
+print(message.startswith('Python'))
+
+print(message.endswith('cool'))
+
+s = "12345678910"
+print(s.isdecimal())
+
+s = "abcfgadf"
+print(s.isdecimal())
+
+s = "hi, lol, funny"
+print(s.isdecimal())
+
+s = "2359832475693"
+print(s.isdecimal())
+
+secrete_message = 'Python is cool and awesome'
+print(print(secrete_message.split(' ')))
+
+import math
+x = math.fabs(-20)
+print(x)
