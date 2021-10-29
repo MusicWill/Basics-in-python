44c7146a49fc24e4ddbc11cc7161ed9d1f1a5205
MusicWill <littlethings0630@gmail.com>
2021-10-29 15:51:54 -0400

    Update test.py

--- test.py
+++ test.py
@@ -21,3 +21,50 @@ while 1234 and 1290<=1289:
    print("good")
 else:
     print("not good")
+
+
+
+#string/math methods
+
+
+
+str = "my name is Willis."
+print(str. capitalize())
+
+str = "Hello."
+print(str.casefold())
+
+str = "what is your name?"
+print(str.capitalize())
+
+message = 'Python is cool'
+print('Number of occurrence of o:', message.count('o'))
+
+print(message.startswith('Python'))
+
+print(message.lower())
+
+print(message.upper())
+
+print(message.startswith('Python'))
+
+print(message.endswith('cool'))
+
+s = "12345678910"
+print(s.isdecimal())
+
+s = "abcfgadf"
+print(s.isdecimal())
+
+s = "hi, lol, funny"
+print(s.isdecimal())
+
+s = "2359832475693"
+print(s.isdecimal())
+
+secrete_message = 'Python is cool and awesome'
+print(print(secrete_message.split(' ')))
+
+import math
+x = math.fabs(-20)
+print(x)
