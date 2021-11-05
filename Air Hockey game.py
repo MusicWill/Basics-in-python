bfd5fe6816810dd2fda6439b5ad8833d828088fa
MusicWill <littlethings0630@gmail.com>
2021-11-05 16:22:30 -0400

    Create Air Hockey game.py

---
+++ Air Hockey game.py
@@ -0,0 +1,9 @@
+#Game: Air Hockey
+
+#Players are the strikers(paddles)
+#The ball(chip)
+#The goal
+#The table
+#In the middle of the table, there is a line.
+#The strikers(paddles) can move up and down, left and right.
+#The strikers(paddles) cannot cross the line in the middle of the table.
